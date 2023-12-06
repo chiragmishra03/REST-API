f798970b140167f8470c2d49b6c7570fe6754ce9
CHIRAG MISHRA <chirag3102001@gmail.com>
2023-12-06 23:25:04 +0530

    "create and get user"

--- routes/users.route.js
+++ routes/users.route.js
@@ -1,7 +1,31 @@
 const router = require("express").Router();
-const user = require("../MOCK_DATA.json");
-router.get("/users", (req, res) => {
-  res.send(user);
+const { dirname, join } = require("path");
+const users = require("../MOCK_DATA.json");
+const fs = require("fs");
+
+router.get("/api/users", (req, res) => {
+  res.send(users);
+});
+
+router.get("/api/user/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const user_data = users.find((user) => user.id == id);
+  res.send(user_data);
+});
+
+router.post("/api/create-user", (req, res) => {
+  let newUser = {};
+  newUser = { id: users.length + 1, ...req.body };
+  users.push(newUser);
+
+  const filePath = join(dirname(__dirname), "MOCK_DATA.json");
+  fs.writeFile(filePath, JSON.stringify(users), (err) => {
+    if (err) {
+      console.error("Error writing to file:", err);
+      return res.status(500).send("Error writing to file");
+    }
+    return res.send("Created Successfully");
+  });
 });
 
 module.exports = router;
