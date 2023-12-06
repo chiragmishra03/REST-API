89463819569ef63a6451b422d8b76331aad020a0
CHIRAG MISHRA <chirag3102001@gmail.com>
2023-12-06 23:26:24 +0530

    "Done"

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const port = 3000;
 const app = express();
-const getusers = require("./routes/users.route");
+const userRoutes = require("./routes/users.route");
 const user = require("./MOCK_DATA.json");
 app.use(express.json());
-app.use(getusers);
+app.use(userRoutes);
 
 app.listen(port, () => {
   console.log("Server is running");
